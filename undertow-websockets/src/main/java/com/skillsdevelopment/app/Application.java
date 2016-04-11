7f5427f871892ed59eff0b641d976a913595efe5
Pär Tjärnberg <par.tjarnberg@cygni.se>
2016-04-11 08:23:43 +0200

    Added logging

--- undertow-websockets/src/main/java/com/skillsdevelopment/app/Application.java
+++ undertow-websockets/src/main/java/com/skillsdevelopment/app/Application.java
@@ -3,6 +3,7 @@ package com.skillsdevelopment.app;
 import com.skillsdevelopment.app.web.WebSocketEndpoint;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
@@ -16,28 +17,29 @@ public class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(final String[] args) throws ServletException {
-        new WebSocketDeploymentInfo().addEndpoint(WebSocketEndpoint.class);
-
-        final DeploymentInfo deployment = Servlets.deployment().setContextPath("/ws")
-                .addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME, new WebSocketDeploymentInfo().addEndpoint(WebSocketEndpoint.class))
+        final DeploymentInfo deployment = Servlets.deployment()
+                .setContextPath("/my-app")
                 .setDeploymentName("my-app.war")
+                .addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME, new WebSocketDeploymentInfo()
+                        .addEndpoint(WebSocketEndpoint.class))
                 .setClassLoader(Application.class.getClassLoader());
 
         final DeploymentManager manager = Servlets.defaultContainer().addDeployment(deployment);
         manager.deploy();
 
+        final PathHandler pathHandler = Handlers.path(Handlers.redirect("/my-app"))
+                .addPrefixPath("/my-app", manager.start());
+
+        LOGGER.info("Starting server, listening on port {}", 8080);
         final Undertow server = Undertow.builder()
                 .addHttpListener(8080, "0.0.0.0")
-                .setHandler(Handlers.path().addPrefixPath("/ws", manager.start()))
+                .setHandler(pathHandler)
                 .build();
         server.start();
 
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                LOGGER.info("Stopping CDI and HTTP server...");
-                server.stop();
-            }
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Stopping CDI and HTTP server...");
+            server.stop();
         }));
     }
 }
--- undertow-websockets/src/main/java/com/skillsdevelopment/app/web/WebSocketEndpoint.java
+++ undertow-websockets/src/main/java/com/skillsdevelopment/app/web/WebSocketEndpoint.java
@@ -1,25 +1,27 @@
 package com.skillsdevelopment.app.web;
 
-import javax.websocket.CloseReason;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/")
+@ServerEndpoint("/ws")
 public class WebSocketEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEndpoint.class);
 
     @OnOpen
     public void connect(final Session session) {
-
+        LOGGER.info("Connected: {}", session);
     }
 
     @OnMessage
     public void message(final String message, final Session session) {
-
+        LOGGER.info("Message: {}, from: {}", message, session);
     }
 
+    @OnClose
     public void close(final CloseReason closeReason, final Session session) {
-
+        LOGGER.info("Connection closed: {}, reason: {}", session, closeReason);
     }
 }
